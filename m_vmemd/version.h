4ccad1651b500236fb7997e386e5135f801a85d2
ufrisk <github_ufrisk@frizk.net>
2022-02-20 21:59:00 +0100

    Version 4.7.6

--- m_vmemd/version.h
+++ m_vmemd/version.h
@@ -3,8 +3,8 @@
 
 #define VERSION_MAJOR               4
 #define VERSION_MINOR               7
-#define VERSION_REVISION            3
-#define VERSION_BUILD               55
+#define VERSION_REVISION            6
+#define VERSION_BUILD               58
 
 #define VER_FILE_DESCRIPTION_STR    "MemProcFS : Plugin vmemd"
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
