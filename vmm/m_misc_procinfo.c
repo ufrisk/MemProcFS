f2cb0d4687c5ee8ad0f82269529b34f4692eb966
ufrisk <github_ufrisk@frizk.net>
2022-12-10 00:58:10 +0100

    Version 5.2.7

--- vmm/m_misc_procinfo.c
+++ vmm/m_misc_procinfo.c
@@ -11,6 +11,10 @@
 
 typedef struct tdOB_MMISCINFO_CONTEXT {
     OB ObHdr;
+    BOOL fAbort;
+    BOOL fCompleted;
+    DWORD dwProgressPercent;
+    VMMDLL_MODULE_ID MID;
     POB_COMPRESSED pDTB;
 } OB_MMISCINFO_CONTEXT, *POB_PMMISCINFO_CONTEXT;
 
@@ -28,7 +32,6 @@ VOID MMiscProcInfo_InitializeDTB(_In_ VMM_HANDLE H, _In_ POB_PMMISCINFO_CONTEXT 
     QWORD vaPfnPteSystem;
     PMMPFN_MAP_ENTRY pPfn;
     PMMPFNOB_MAP pObPfnMap = NULL;
-    PMMPFNOB_MAP pObPfnMap2 = NULL;
     PVMM_PROCESS pObProcess = NULL;
     PVMM_PROCESS pObSystemProcess = NULL;
     POB_MAP pmObProcessDTB_Kernel = NULL, pmObProcessDTB_User = NULL;
@@ -42,7 +45,7 @@ VOID MMiscProcInfo_InitializeDTB(_In_ VMM_HANDLE H, _In_ POB_PMMISCINFO_CONTEXT 
     vaPfnPteSystem = pObPfnMap->pMap[0].vaPte;
     Ob_DECREF_NULL(&pObPfnMap);
     // 3: Create DTB to Process Map:
-    while((pObProcess = VmmProcessGetNext(H, pObProcess, 0))) {
+    while((pObProcess = VmmProcessGetNext(H, pObProcess, VMM_FLAG_PROCESS_SHOW_TERMINATED))) {
         if(pObProcess->paDTB_Kernel) { ObMap_Push(pmObProcessDTB_Kernel, pObProcess->paDTB_Kernel >> 12, pObProcess); }
         if(pObProcess->paDTB_UserOpt) { ObMap_Push(pmObProcessDTB_User, pObProcess->paDTB_UserOpt >> 12, pObProcess); }
     }
@@ -50,7 +53,8 @@ VOID MMiscProcInfo_InitializeDTB(_In_ VMM_HANDLE H, _In_ POB_PMMISCINFO_CONTEXT 
     cPfnMax = (DWORD)(H->dev.paMax >> 12);
     for(oPfn = 0; oPfn < cPfnMax; oPfn += 0x20000) {
         cPfn = min(0x20000, cPfnMax - oPfn);
-        if(H->fAbort) { goto fail; }
+        if(H->fAbort || ctx->fAbort) { goto fail; }
+        ctx->dwProgressPercent = (DWORD)((100ULL * oPfn) / cPfnMax);
         if(!MmPfn_Map_GetPfn(H, oPfn, cPfn, &pObPfnMap, FALSE)) { goto fail; }
         for(i = 0; i < pObPfnMap->cMap; i++) {
             pPfn = pObPfnMap->pMap + i;
@@ -58,17 +62,14 @@ VOID MMiscProcInfo_InitializeDTB(_In_ VMM_HANDLE H, _In_ POB_PMMISCINFO_CONTEXT 
                 if(oText > 0x00100000 - 0x1000) { goto fail; }
                 if(!VmmRead(H, NULL, (QWORD)pPfn->dwPfn << 12, pbDTB, 0x1000)) { continue; }
                 if(!VmmTlbPageTableVerify(H, pbDTB, (QWORD)pPfn->dwPfn << 12, TRUE)) { continue; }
-                Ob_DECREF_NULL(&pObPfnMap2);
-                if(!MmPfn_Map_GetPfn(H, pPfn->dwPfn, 1, &pObPfnMap2, TRUE)) { continue; }
                 Ob_DECREF_NULL(&pObProcess);
                 pObProcess = ObMap_GetByKey(pmObProcessDTB_Kernel, pPfn->dwPfn);
                 if(!pObProcess) {
                     pObProcess = ObMap_GetByKey(pmObProcessDTB_User, pPfn->dwPfn);
                 }
-                oText += _snprintf_s(uszText + oText, MAX_PATH, _TRUNCATE, "%04x%7i%7i %16llx %16llx %s\n",
+                oText += _snprintf_s(uszText + oText, MAX_PATH, _TRUNCATE, "%04x%7i %16llx %16llx %s\n",
                     cEntries++,
                     pObProcess ? pObProcess->dwPID : 0,
-                    pObPfnMap2->pMap[0].AddressInfo.dwPid,
                     (QWORD)pPfn->dwPfn << 12,
                     pObProcess ? pObProcess->win.EPROCESS.va : 0,
                     pObProcess ? pObProcess->szName : "---"
@@ -77,15 +78,27 @@ VOID MMiscProcInfo_InitializeDTB(_In_ VMM_HANDLE H, _In_ POB_PMMISCINFO_CONTEXT 
         }
         Ob_DECREF_NULL(&pObPfnMap);
     }
+    ctx->dwProgressPercent = 100;
+    ctx->fCompleted = TRUE;
     ctx->pDTB = ObCompress_NewFromStrA(H, H->vmm.pObCacheMapObCompressedShared, uszText);
 fail:
     Ob_DECREF(pmObProcessDTB_Kernel);
     Ob_DECREF(pmObProcessDTB_User);
     Ob_DECREF(pObSystemProcess);
     Ob_DECREF(pObProcess);
-    Ob_DECREF(pObPfnMap2);
     Ob_DECREF(pObPfnMap);
     LocalFree(uszText);
+}
+
+/*
+* Iterate over all process in a separate async thread.
+*/
+VOID MMiscProcInfo_Process_ThreadProc(_In_ VMM_HANDLE H, _In_ POB_PMMISCINFO_CONTEXT ctx)
+{
+    VMMSTATISTICS_LOG Statistics = { 0 };
+    VmmStatisticsLogStart(H, ctx->MID, LOGLEVEL_6_TRACE, NULL, &Statistics, "GET_CONTEXT");
+    MMiscProcInfo_InitializeDTB(H, ctx);
+    VmmStatisticsLogEnd(H, &Statistics, "GET_CONTEXT");
 }
 
 /*
@@ -100,17 +113,14 @@ POB_PMMISCINFO_CONTEXT MMiscProcInfo_GetContext(_In_ VMM_HANDLE H, _In_ PVMMDLL_
 {
     static SRWLOCK LockSRW = SRWLOCK_INIT;
     POB_PMMISCINFO_CONTEXT pObInfo;
-    VMMSTATISTICS_LOG Statistics = { 0 };
     if((pObInfo = (POB_PMMISCINFO_CONTEXT)ObContainer_GetOb((POB_CONTAINER)ctxP->ctxM))) { return pObInfo; }
     AcquireSRWLockExclusive(&LockSRW);
     if(!(pObInfo = (POB_PMMISCINFO_CONTEXT)ObContainer_GetOb((POB_CONTAINER)ctxP->ctxM))) {
-        VmmStatisticsLogStart(H, ctxP->MID, LOGLEVEL_6_TRACE, NULL, &Statistics, "GET_CONTEXT");
-        // process:
         if((pObInfo = Ob_AllocEx(H, OB_TAG_CTX_MMISCINFO, LMEM_ZEROINIT, sizeof(OB_MMISCINFO_CONTEXT), (OB_CLEANUP_CB)MMiscProcInfo_Context_CallbackCleanup, NULL))) {
-            MMiscProcInfo_InitializeDTB(H, pObInfo);
+            pObInfo->MID = ctxP->MID;
             ObContainer_SetOb((POB_CONTAINER)ctxP->ctxM, pObInfo);
+            VmmWork_Ob(H, (PVMM_WORK_START_ROUTINE_OB_PFN)MMiscProcInfo_Process_ThreadProc, (POB)pObInfo, 0, VMMWORK_FLAG_PRIO_NORMAL);
         }
-        VmmStatisticsLogEnd(H, &Statistics, "GET_CONTEXT");
     }
     ReleaseSRWLockExclusive(&LockSRW);
     return pObInfo;
@@ -121,6 +131,9 @@ NTSTATUS MMiscProcInfo_Read(_In_ VMM_HANDLE H, _In_ PVMMDLL_PLUGIN_CONTEXT ctxP,
     NTSTATUS nt = VMMDLL_STATUS_FILE_INVALID;
     POB_PMMISCINFO_CONTEXT ctxOb = MMiscProcInfo_GetContext(H, ctxP);
     if(ctxOb) {
+        if(!_stricmp(ctxP->uszPath, "progress_percent.txt")) {
+            return Util_VfsReadFile_FromNumber(ctxOb->dwProgressPercent, pb, cb, pcbRead, cbOffset);
+        }
         if(!_stricmp(ctxP->uszPath, "dtb.txt")) {
             nt = Util_VfsReadFile_FromObCompressed(ctxOb->pDTB, pb, cb, pcbRead, cbOffset);
         }
@@ -131,8 +144,11 @@ NTSTATUS MMiscProcInfo_Read(_In_ VMM_HANDLE H, _In_ PVMMDLL_PLUGIN_CONTEXT ctxP,
 
 BOOL MMiscProcInfo_List(_In_ VMM_HANDLE H, _In_ PVMMDLL_PLUGIN_CONTEXT ctxP, _Inout_ PHANDLE pFileList)
 {
+    DWORD cbProgress;
     POB_PMMISCINFO_CONTEXT ctxOb = MMiscProcInfo_GetContext(H, ctxP);
     if(ctxOb && !ctxP->uszPath[0]) {
+        cbProgress = (ctxOb->dwProgressPercent == 100) ? 3 : ((ctxOb->dwProgressPercent >= 10) ? 2 : 1);
+        VMMDLL_VfsList_AddFile(pFileList, "progress_percent.txt", cbProgress, NULL);
         VMMDLL_VfsList_AddFile(pFileList, "dtb.txt", ObCompress_Size(ctxOb->pDTB), NULL);
     }
     Ob_DECREF(ctxOb);
@@ -141,13 +157,23 @@ BOOL MMiscProcInfo_List(_In_ VMM_HANDLE H, _In_ PVMMDLL_PLUGIN_CONTEXT ctxP, _In
 
 VOID MMiscProcInfo_Notify(_In_ VMM_HANDLE H, _In_ PVMMDLL_PLUGIN_CONTEXT ctxP, _In_ DWORD fEvent, _In_opt_ PVOID pvEvent, _In_opt_ DWORD cbEvent)
 {
+    POB_PMMISCINFO_CONTEXT ctxOb;
     if(fEvent == VMMDLL_PLUGIN_NOTIFY_REFRESH_SLOW) {
-        ObContainer_SetOb((POB_CONTAINER)ctxP->ctxM, NULL);
+        if((ctxOb = MMiscProcInfo_GetContext(H, ctxP))) {
+            ctxOb->fAbort = TRUE;
+            Ob_DECREF(ctxOb);
+            ObContainer_SetOb((POB_CONTAINER)ctxP->ctxM, NULL);
+        }
     }
 }
 
 VOID MMiscProcInfo_Close(_In_ VMM_HANDLE H, _In_ PVMMDLL_PLUGIN_CONTEXT ctxP)
 {
+    POB_PMMISCINFO_CONTEXT ctxOb;
+    if((ctxOb = MMiscProcInfo_GetContext(H, ctxP))) {
+        ctxOb->fAbort = TRUE;
+        Ob_DECREF(ctxOb);
+    }
     Ob_DECREF(ctxP->ctxM);
 }
 
