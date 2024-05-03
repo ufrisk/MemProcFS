8784241745ca14e942c31574d3541282fd58bbd3
Ulf Frisk <github_ufrisk@frizk.net>
2024-05-04 01:07:13 +0200

    Version 5.9.12

--- vmmpyc/version.h
+++ vmmpyc/version.h
@@ -3,8 +3,8 @@
 
 #define VERSION_MAJOR               5
 #define VERSION_MINOR               9
-#define VERSION_REVISION            11
-#define VERSION_BUILD               158
+#define VERSION_REVISION            12
+#define VERSION_BUILD               159
 
 #define VER_FILE_DESCRIPTION_STR    "MemProcFS : Python API"
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
