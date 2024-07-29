0b18890e33ac0d87a3437abc5b2c473edf12c0d0
Ulf Frisk <github_ufrisk@frizk.net>
2024-07-30 00:25:06 +0200

    Version 5.11.1

--- m_vmemd/version.h
+++ m_vmemd/version.h
@@ -3,8 +3,8 @@
 
 #define VERSION_MAJOR               5
 #define VERSION_MINOR               11
-#define VERSION_REVISION            0
-#define VERSION_BUILD               168
+#define VERSION_REVISION            1
+#define VERSION_BUILD               169
 
 #define VER_FILE_DESCRIPTION_STR    "MemProcFS : Plugin vmemd"
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
--- vmmsharp/vmmsharp/Properties/AssemblyInfo.cs
+++ vmmsharp/vmmsharp/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.Versioning;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.11.0.168")]
-[assembly: AssemblyFileVersion("5.11.0.168")]
+[assembly: AssemblyVersion("5.11.1.169")]
+[assembly: AssemblyFileVersion("5.11.1.169")]
